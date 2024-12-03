6b911185ddf74da81400dc218f44df9bc6ff6b89
Anirudh Goel <ani050599@gmail.com>
2024-12-03 23:02:04 +0530

    Locking demo (#3)
    * initial commit
    
    * added comment

--- locking-optimistic/src/main/java/org/example/RedisClient.java
+++ locking-optimistic/src/main/java/org/example/RedisClient.java
@@ -24,6 +24,12 @@ public class RedisClient {
 
     public boolean setIfNotExist(String key, UUID secretValue)
     {
+        SetParams params = new SetParams().nx().px(2000);
+        // The above will ensure that set will succeed only if key doesn't exist.
+        String response = jedis.set(key, secretValue.toString(), params);
+
+        if ("OK".equals(response)) {
+            System.out.println("Lock acquired with ID: " + secretValue);
         String uniqueId = UUID.randomUUID().toString(); // Generate a unique ID for this lock
         SetParams params = new SetParams().nx().px(2000);
         String response = jedis.set(key, secretValue.toString(), params);
@@ -37,12 +43,15 @@ public class RedisClient {
 
     public boolean deleteIfValueSame(String key, UUID secretValue)
     {
+        // Lua script is a redis feature which ensures that the given script executes atomically.
         String luaScript =
                 "if redis.call('GET', KEYS[1]) == ARGV[1] then " +
                         "    return redis.call('DEL', KEYS[1]) " +
                         "else " +
                         "    return 0 " +
                         "end";
+        // In the above script we allow to delete the key only if the value provided matches the existing value,
+        // this ensures that only the client who acquired the lock (and is hence aware of the secret value) can release it
 
         Object result = jedis.eval(luaScript, 1, key, secretValue.toString());
         if (result.equals(1L)) {
